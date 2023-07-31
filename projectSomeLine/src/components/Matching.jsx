c00c91b679a38d1b9fcd87a914b3c60000ca9d63
leechanyongyong <goodleecy1989@gmail.com>
2023-07-31 19:24:50 +0900

    7.31.19.lee

--- projectSomeLine/src/components/Matching.jsx
+++ projectSomeLine/src/components/Matching.jsx
@@ -99,7 +99,7 @@ const Matching = () => {
           <button className='mat_popup_close' onClick={matClosePopup}>X</button>
           <div className='mat_popup_text'>
             <h4>💬 알림.</h4>
-            <p>이찬용님, 설레이는 새로운 메세지가 도착했어요!</p>
+            <p>이찬용님, 새로운 매칭이 도착했어요!</p>
           </div>
         </div>
         <div className="login_bgm_b">
