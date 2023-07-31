4ebaa54580c6c24ec3b9455450f62bfc4e50517e
AndroidYoungChan <lych96@gmail.com>
2023-07-31 21:58:08 +0900

    7.31.10.lim

--- projectSomeLine/src/components/Profile.jsx
+++ projectSomeLine/src/components/Profile.jsx
@@ -11,18 +11,40 @@ import {
   onSnapshot,
   query,
   orderBy,
-  getDocs
+  getDoc,
+  getDocs,
 } from "firebase/firestore";
+import { getStorage, ref, getDownloadURL} from "firebase/storage"
 
 const Profile = () => {
 
-    const { currentUser } = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
+  const [user, setUser] = useState(null);
 
-  
-    const userRef = collection(db, "users") 
-    
-  
-    
+  useEffect(() => {
+    if (currentUser && currentUser.email) {
+      const q = query(collection(db, "users"), where("id", "==", currentUser.email));
+      getDocs(q).then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          setUser(doc.data());
+        });
+      });
+    }
+  }, [])
+
+  // const storage = getStorage();
+  // getDownloadURL(ref(storage, user.profileUrl))
+  //   .then((url) => {
+  //     const img = document.getElementById('myimg');
+  //     img.setAttribute('src', url);
+  //     console.log(url);
+  //   })
+  //   .catch((error) => {
+  //     alert(`에러 : ${error}`)
+  //   });
+   
+
+
   
     return (
       <div className='matching_bg'>
@@ -34,11 +56,12 @@ const Profile = () => {
           <div className='matching_in_box'>
                   <div className='mat_info_card'>
                     <div className='info_img_box'>
+                      <img id='myimg'/>
                     </div>
                     <div className='info_info_box'>
                         <>
-                          <p>이름:</p>
-                          <p>나이:</p>
+                          <p>이름: {user.name}</p>
+                          <p>나이: {user.age}</p>
                         </>
                     </div>
                   </div>
