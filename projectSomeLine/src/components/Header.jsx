422a759195cdf9ad21b1baf96a26da09b8ba7b66
leechanyongyong <goodleecy1989@gmail.com>
2023-07-31 19:37:46 +0900

    7.31.19.lee

--- projectSomeLine/src/components/Header.jsx
+++ projectSomeLine/src/components/Header.jsx
@@ -9,24 +9,18 @@ import { auth } from '../firebase-config';
 // 다만 프로젝트가 종료될 시까지 사용 안 할 시 마무리에 삭제하려고 합니다. -작업자 : 이찬용
 
 const Header = () => {
-    // const menuItems = ['Menu1', 'Menu2', 'Menu3', 'Menu4'];
 
     const { currentUser } = useContext(AuthContext);
 
     return (
         <header className='top'>
+            {/* 탑 해드 오른쪽 공간과 버튼 "SOME.LINE" */}
             <div className='top_logo_left'>
                 <Link to={currentUser === null ? '/' : '/chatlist'} className='top_logo_text_left'>SOME.LINE</Link>
             </div>
+
+            {/* 탑 해드 왼쪽 공간과 버튼 "로그인, 가입 버튼" */}
             <ul className='top_nav_right'>
-                {/* {menuItems.map((menuItem, index) => (
-                    <li key={index} className='list_Style menuicon'>
-                        <a href="#">{menuItem}</a>
-                    </li>
-                ))} */}
-                {/* <li className='list_Style'>
-                    <a href="#" className='nav_login_btn'>Login</a>
-                </li> */}
                 <li className='list_Style'>
                     <Link to="/signup" 
                         className={currentUser === null ? 'nav_login_btn' : 'hidden'}>Sign up</Link>
