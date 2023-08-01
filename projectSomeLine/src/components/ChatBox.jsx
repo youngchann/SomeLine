9fae85fe89527e74adc9f9709e69ba002c713514
AndroidYoungChan <lych96@gmail.com>
2023-08-01 15:33:11 +0900

    8.1.15.lim

--- projectSomeLine/src/components/ChatBox.jsx
+++ projectSomeLine/src/components/ChatBox.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react';
-
+import React, { useState, useEffect, useContext} from 'react';
+import { AuthContext } from "../context/AuthContext";
 import { db, auth } from "../firebase-config";
 import {
   collection,
@@ -11,36 +11,29 @@ import {
   orderBy,
 } from "firebase/firestore";
 
-const ChatBox = ({ room }) => {
+const ChatBox = ({room}) => {
+
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
   const messagesRef = collection(db, "messages");
-  const messagesEndRef = useRef(null);
+  const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
-    // 스크롤바를 항상 가장 아래로 내리는 함수
-    const scrollToBottom = () => {
-      messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-    };
-
     const queryMessages = query(
       messagesRef,
       where("room", "==", "여행 좋아하는 남자"),
       orderBy("createdAt")
     );
-
-    const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
+    const unsuscribe = onSnapshot(queryMessages, (snapshot) => {
       let messages = [];
       snapshot.forEach((doc) => {
         messages.push({ ...doc.data(), id: doc.id });
       });
       console.log(messages);
       setMessages(messages);
-      // 새로운 메시지가 추가되면 스크롤바를 아래로 내림
-      scrollToBottom();
     });
 
-    return () => unsubscribe();
+    return () => unsuscribe();
   }, []);
 
   const handleSubmit = async (event) => {
@@ -51,7 +44,7 @@ const ChatBox = ({ room }) => {
       text: newMessage,
       createdAt: serverTimestamp(),
       user: auth.currentUser.displayName,
-      room: "여행 좋아하는 남자"
+      room : "여행 좋아하는 남자"
     });
 
     setNewMessage("");
@@ -60,23 +53,19 @@ const ChatBox = ({ room }) => {
   return (
     <div className='chatbox_bg'>
       <div className="login_bgm_b">
+        {/* <img src="img/main_photo.jpeg" type='video/mp4' /> */}
         <video className="login_bgm" autoPlay muted loop>
-          <source src='videos/mainmain8.mp4' type='video/mp4' />
+            <source src='videos/mainmain8.mp4' type='video/mp4' />
         </video>
       </div>
-      <div className='you_chat_Profil'>
-        <div className='chat_Profil_img'></div>
-        <h2 className='you_chat_Profil_name'>상대방</h2>
-      </div>
+      <div className='you_chat_Profil'><div className='chat_Profil_img'></div><h2 className='you_chat_Profil_name'>상대방</h2></div>
       <div className='chatbox_box'>
         <div className='messages'>
           {messages.map((message) => (
             <div key={message.id} className={`message ${message.user === auth.currentUser.displayName ? "my-message" : "other-message"}`}>
-              <div className='chatbox_talk_box'><span className="user">{message.text}</span> </div>
+                <div className='chatbox_talk_box'><span className="user">{message.text}</span> </div>
             </div>
           ))}
-          {/* 스크롤바를 항상 아래로 내리는 빈 div */}
-          <div ref={messagesEndRef} />
         </div>
         <form className='chatbox_input' onSubmit={handleSubmit}>
           <input 
@@ -95,15 +84,16 @@ const ChatBox = ({ room }) => {
       </div>
       <div className='my_chat_Profil'>
         <div  className='chat_Profil_img'></div> 
-        <h2 className='my_chat_Profil_name'>{auth.currentUser.displayName}</h2>
+        <h2 className='my_chat_Profil_name'>{currentUser.displayName}</h2>
         <div className='imotion_box'>
           <button className='imotion_btn'>💕좋아</button>
           <button className='imotion_btn'>😢슬퍼</button>
           <button className='imotion_btn'>👿화나</button>
         </div>
       </div>
+
     </div>
-  );
-};
+  )
+}
 
 export default ChatBox;
