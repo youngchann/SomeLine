654e0abeb41fca3542c107b08f30116aae804ead
Jiho <utop402@gmail.com>
2023-08-01 19:23:22 +0900

    8.1.19 Kook

--- projectSomeLine/src/components/ChatBox.jsx
+++ projectSomeLine/src/components/ChatBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect} from 'react';
 
 import { db, auth } from "../firebase-config";
 import {
@@ -11,35 +11,28 @@ import {
   orderBy,
 } from "firebase/firestore";
 
-const ChatBox = ({ room }) => {
+const ChatBox = ({room}) => {
+
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
   const messagesRef = collection(db, "messages");
-  const [showLove, setShowLove] = useState(false);
-  const messagesEndRef = useRef(null);
 
   useEffect(() => {
-    const scrollToBottom = () => {
-      messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-    };
-
     const queryMessages = query(
       messagesRef,
       where("room", "==", "여행 좋아하는 남자"),
       orderBy("createdAt")
     );
-
-    const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
+    const unsuscribe = onSnapshot(queryMessages, (snapshot) => {
       let messages = [];
       snapshot.forEach((doc) => {
         messages.push({ ...doc.data(), id: doc.id });
       });
       console.log(messages);
       setMessages(messages);
-      scrollToBottom();
     });
 
-    return () => unsubscribe();
+    return () => unsuscribe();
   }, []);
 
   const handleSubmit = async (event) => {
@@ -49,37 +42,59 @@ const ChatBox = ({ room }) => {
     await addDoc(messagesRef, {
       text: newMessage,
       createdAt: serverTimestamp(),
-      user: auth.currentUser ? auth.currentUser.displayName : "Anonymous",
-      room: "여행 좋아하는 남자"
+      user: auth.currentUser.displayName,
+      room : "여행 좋아하는 남자"
     });
 
     setNewMessage("");
   };
+  // 이모션이 작동하는 코드입니다.
+  // 현재 해당 코드는 챗 지피티를 활용하여 만들어 졌으나, 비효율적입니다.
+  const [hartClicked, hartIsClicked] = useState(false);
+  const [sadClicked, sadIsClicked] = useState(false);
+  const [angryClicked, angryIsClicked] = useState(false);
+  const [key, setKey] = useState(Math.random());
 
-  const handleLoveClick = () => {
-    setShowLove(true);
-    setTimeout(() => setShowLove(false), 5000);
+  const hart_Click = () => {
+    hartIsClicked(true);
+    sadIsClicked(false);
+    angryIsClicked(false);
+    setKey(Math.random()); 
+    setTimeout(() => hartIsClicked(false), 3000);
   };
+  const sad_Click = () => {
+    hartIsClicked(false);
+    sadIsClicked(true);
+    angryIsClicked(false);
+    setKey(Math.random()); 
+    setTimeout(() => sadIsClicked(false), 3000);
+  };
+  const angry_Click = () => {
+    hartIsClicked(false);
+    sadIsClicked(false);
+    angryIsClicked(true);
+    setKey(Math.random()); 
+    setTimeout(() => angryIsClicked(false), 3000);
+  };;
+  //  챗지피티 이모션 활용끝
+
 
   return (
     <div className='chatbox_bg'>
       <div className="login_bgm_b">
+        {/* <img src="img/main_photo.jpeg" type='video/mp4' /> */}
         <video className="login_bgm" autoPlay muted loop>
-          <source src='videos/mainmain8.mp4' type='video/mp4' />
+            <source src='videos/mainmain8.mp4' type='video/mp4' />
         </video>
       </div>
-      <div className='you_chat_Profil'>
-        <div className='chat_Profil_img'></div>
-        <h2 className='you_chat_Profil_name'>상대방</h2>
-      </div>
+      <div className='you_chat_Profil'><div className='chat_Profil_img'></div><h2 className='you_chat_Profil_name'>상대방</h2></div>
       <div className='chatbox_box'>
         <div className='messages'>
           {messages.map((message) => (
-            <div key={message.id} className={`message ${message.user === (auth.currentUser ? auth.currentUser.displayName : "Anonymous") ? "my-message" : "other-message"}`}>
-              <div className='chatbox_talk_box'><span className="user">{message.text}</span> </div>
+            <div key={message.id} className={`message ${message.user === auth.currentUser.displayName ? "my-message" : "other-message"}`}>
+                <div className='chatbox_talk_box'><span className="user">{message.text}</span> </div>
             </div>
           ))}
-          <div ref={messagesEndRef} />
         </div>
         <form className='chatbox_input' onSubmit={handleSubmit}>
           <input 
@@ -97,17 +112,23 @@ const ChatBox = ({ room }) => {
         </form>
       </div>
       <div className='my_chat_Profil'>
-        <div className="chat_Profil_img_emotion">{showLove && '💕'}</div>
-        <div className='chat_Profil_img'></div> 
-        <h2 className='my_chat_Profil_name'>{auth.currentUser ? auth.currentUser.displayName : "Anonymous"}</h2>
+        <div  className='chat_Profil_img'></div> 
+        <h2 className='my_chat_Profil_name'>{auth.currentUser.displayName}</h2>
+        {/* 해당 코드는 하트 이모션이 올라옵니다. */}
+        <div key={key} className={`emt_hart ${hartClicked ? 'moveFadeOut' : ''}`}>💕</div>
+        <div key={key} className={`emt_sad ${sadClicked ? 'moveFadeOut' : ''}`}>😢</div>
+        <div key={key} className={`emt_angry ${angryClicked ? 'moveFadeOut' : ''}`}>👿</div>
+        
         <div className='imotion_box'>
-          <button className='imotion_btn' onClick={handleLoveClick}>💕좋아</button>
-          <button className='imotion_btn'>😢슬퍼</button>
-          <button className='imotion_btn'>👿화나</button>
+
+          <button className='imotion_btn_hart_btn' onClick={hart_Click}>💕좋아</button>
+          <button className='imotion_btn_sad_btn' onClick={sad_Click}>😢슬퍼</button>
+          <button className='imotion_btn_angry_btn' onClick={angry_Click}>👿화나</button>
         </div>
       </div>
+
     </div>
-  );
-};
+  )
+}
 
 export default React.memo(ChatBox);
