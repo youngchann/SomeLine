580616169fb231d63ed3a5645e6a01f5ba46bb5d
AndroidYoungChan <lych96@gmail.com>
2023-08-01 09:25:15 +0900

    8.1.9.25.lim

--- projectSomeLine/src/components/Profile.jsx
+++ projectSomeLine/src/components/Profile.jsx
@@ -3,7 +3,6 @@ import 'swiper/css';
 import 'swiper/css/effect-cards';
 import { AuthContext } from "../context/AuthContext";
 
-
 import { db } from "../firebase-config";
 import {
   collection,
@@ -14,10 +13,9 @@ import {
   getDoc,
   getDocs,
 } from "firebase/firestore";
-import { getStorage, ref, getDownloadURL} from "firebase/storage"
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 const Profile = () => {
-
   const { currentUser } = useContext(AuthContext);
   const [user, setUser] = useState(null);
 
@@ -30,45 +28,52 @@ const Profile = () => {
         });
       });
     }
-  }, [])
+  }, [currentUser]);
 
-  // const storage = getStorage();
-  // getDownloadURL(ref(storage, user.profileUrl))
-  //   .then((url) => {
-  //     const img = document.getElementById('myimg');
-  //     img.setAttribute('src', url);
-  //     console.log(url);
-  //   })
-  //   .catch((error) => {
-  //     alert(`에러 : ${error}`)
-  //   });
-   
+  useEffect(() => {
+    // 이미지 가져오는 코드가 있을 때만 실행하도록 수정
+    if (user && user.profileUrl) {
+      const storage = getStorage();
+      getDownloadURL(ref(storage, user.profileUrl))
+        .then((url) => {
+          const img = document.getElementById('myimg');
+          img.setAttribute('src', url);
+          console.log(url);
+        })
+        .catch((error) => {
+          alert(`에러 : ${error}`);
+        });
+    }
+  }, [user]);
 
+  // user가 null인 경우, Loading 메시지를 표시
+  if (!user) {
+    return <p>Loading...</p>;
+  }
 
-  
-    return (
-      <div className='matching_bg'>
-          <div className="login_bgm_b">
-              <video className="login_bgm" autoPlay muted loop>
-                <source src='videos/mainmain10.mp4' type='video/mp4' />
-              </video>
-          </div>
-          <div className='matching_in_box'>
-                  <div className='mat_info_card'>
-                    <div className='info_img_box'>
-                      <img id='myimg'/>
-                    </div>
-                    <div className='info_info_box'>
-                        <>
-                          <p>이름: {user.name}</p>
-                          <p>나이: {user.age}</p>
-                        </>
-                    </div>
-                  </div>
-          </div>
+  // user가 null이 아닌 경우, 프로필 정보를 화면에 표시
+  return (
+    <div className='matching_bg'>
+      <div className="login_bgm_b">
+        <video className="login_bgm" autoPlay muted loop>
+          <source src='videos/mainmain10.mp4' type='video/mp4' />
+        </video>
       </div>
-    )
-}
+      <div className='matching_in_box'>
+        <div className='mat_info_card'>
+          <div className='info_img_box'>
+            <img id='myimg' src={user.profileUrl} alt="User Profile" />
+          </div>
+          <div className='info_info_box'>
+            <>
+              <p>이름: {user.name}</p>
+              <p>나이: {user.age}</p>
+            </>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-export default Profile
+export default Profile;
