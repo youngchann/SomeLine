5a673d2b96682d7ee7e6716c3a6e996c106b9a03
leechanyongyong <goodleecy1989@gmail.com>
2023-08-15 12:48:01 +0900

    8.15.12.lee

--- projectSomeLine/src/components/CommunitySpace.jsx
+++ projectSomeLine/src/components/CommunitySpace.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect, useRef, useContext } from 'react';
+import VanillaTilt from 'vanilla-tilt';
+
+
+function Tilt(props) {
+  const { options, ...rest } = props;
+  const tilt = useRef(null);
+
+  useEffect(() => {
+    VanillaTilt.init(tilt.current, options);
+  }, [options]);
+
+  return <div ref={tilt} {...rest} />;
+}
 
 const CommunitySpace = () => {
+
+  const options = {
+    scale: 1.05,
+    speed: 1000,
+    max: 4
+  };
+
+
   return (
     <div className='communityspace_bg'>
       <div className="login_bgm_b">
@@ -14,18 +35,83 @@ const CommunitySpace = () => {
         
         <div className='communityspace_box_in_box2'>
           <div className='communityspace_box_in_best_page'>
-            <div>하나</div>
-            <div>하나</div>
-            <div>하나</div>
-            <div>하나</div>
-            <div>하나</div>
-            <div>하나</div>
-            <div>하나</div>
-            <div>하나</div>
-            <div>하나</div>
+          <div className='comu_best_box_header'>오늘의 베스트 글</div>
+            <div className='comu_best_box_contents'>어제 이런 일이 있었어요.</div>
+            <div className='comu_best_box_contents'>전기버스에서 너무 냄새나요 ㅜ</div>
+            <div className='comu_best_box_contents'>오늘 좋은 일이 있었어요!</div>
+            <div className='comu_best_box_contents'>연애 시작했어요!</div>
+            <div className='comu_best_box_contents'>우울해요.</div>
+            <div className='comu_best_box_contents'>세상 믿을 사람 하나 없네요.</div>
+            <div className='comu_best_box_contents'>임영찬이라는 사람 아세요?</div>
+            <div className='comu_best_box_contents'>국지호가 복싱에서 우승했어요</div>
+          </div>
+          <div className='communityspace_box_in_best_page'>
+            <div className='comu_best_box_header'>어제의 베스트 글</div>
+            <div className='comu_best_box_contents'>전도희씨 안녕하세요?</div>
+            <div className='comu_best_box_contents'>좋은 일이 생겼어요.</div>
+            <div className='comu_best_box_contents'>이글 누르면 여자친구 생긴다.</div>
+            <div className='comu_best_box_contents'>콘칩 안에서 이물질이 나왔어요ㅠ</div>
+            <div className='comu_best_box_contents'>코딩 재밌어요.</div>
+            <div className='comu_best_box_contents'>이찬용팬은 이글 꼭 보세여.</div>
+            <div className='comu_best_box_contents'>서울 맛집 빵집 베스트 10위</div>
+            <div className='comu_best_box_contents'>이런 사람 조심하세요.</div>
           </div>
         </div>
-
+        <div class="communityspace_animated-title">
+          <div class="communityspace_title_track">
+            <div class="communityspace_title_content">WE COME TO LOVE NOT BY FINDING A PERFECT PERSON, BUT BY LEARNING TO SEE AN IMPERFECT PERSON PERFECTLY.</div>
+          </div>
+        </div>
+        <div className='communityspace_box_in_box3'>
+          <Tilt options={options} className='comu_boxs'>
+              <div className='comu_boxs_icon'>💕</div>
+              <div className='comu_boxs_title'>썸·연애</div>
+          </Tilt>
+          <Tilt options={options} className='comu_boxs'>
+              <div className='comu_boxs_icon'>🔥</div>
+              <div className='comu_boxs_title'>인기·핫픽</div>
+          </Tilt>
+          <Tilt options={options} className='comu_boxs'>
+              <div className='comu_boxs_icon'>🎧</div>
+              <div className='comu_boxs_title'>음악</div>
+          </Tilt>
+          <Tilt options={options} className='comu_boxs'>
+              <div className='comu_boxs_icon'>🐩</div>
+              <div className='comu_boxs_title'>반려 동물</div>
+          </Tilt>
+          <Tilt options={options} className='comu_boxs'>
+              <div className='comu_boxs_icon'>🏙</div>
+              <div className='comu_boxs_title'>회사 생활</div>
+          </Tilt>
+          <Tilt options={options} className='comu_boxs'>
+              <div className='comu_boxs_icon'>🎳</div>
+              <div className='comu_boxs_title'>취미 생활</div>
+          </Tilt>
+          <Tilt options={options} className='comu_boxs'>
+              <div className='comu_boxs_icon'>🛍</div>
+              <div className='comu_boxs_title'>지름·쇼핑</div>
+          </Tilt>
+          <Tilt options={options} className='comu_boxs'>
+              <div className='comu_boxs_icon'>💋</div>
+              <div className='comu_boxs_title'>패션·뷰티</div>
+          </Tilt>
+          <Tilt options={options} className='comu_boxs'>
+              <div className='comu_boxs_icon'>💌</div>
+              <div className='comu_boxs_title'>셀소 공간</div>
+          </Tilt>
+          <Tilt options={options} className='comu_boxs'>
+              <div className='comu_boxs_icon'>🎮</div>
+              <div className='comu_boxs_title'>게임</div>
+          </Tilt>
+          <Tilt options={options} className='comu_boxs'>
+              <div className='comu_boxs_icon'>🗽</div>
+              <div className='comu_boxs_title'>여행·맛집</div>
+          </Tilt>
+          <Tilt options={options} className='comu_boxs'>
+              <div className='comu_boxs_icon'>🏄🏻‍♂️</div>
+              <div className='comu_boxs_title'>다이어트·헬스</div>
+          </Tilt>
+        </div>
       </div>
     </div>
   )
--- projectSomeLine/src/components/SideMenu.jsx
+++ projectSomeLine/src/components/SideMenu.jsx
@@ -1,33 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const SideMenu = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className='side_menu_background'>
-        <div className='side_menu_box'>
-            <ul className='side_menu_list'>
-                <Link to='/matching' className='side_menus_menu matching'>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    매칭</Link>
-                <Link to='/chatlist' className='side_menus_menu chatlist'>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    대화방</Link>
-                <Link to='/profile' className='side_menus_menu chatbot'>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    프로필</Link>
-            </ul>
-        </div>
+      <div className='side_menu_box'>
+        <ul className='side_menu_list'>
+          <Link to='/matching' className='side_menus_menu matching'>
+            {isMobile ? <span className="icon">💞</span> : <span className="text">매칭</span>}
+          </Link>
+          <Link to='/chatlist' className='side_menus_menu chatlist'>
+            {isMobile ? <span className="icon">💬</span> : <span className="text">대화방</span>}
+          </Link>
+          <Link to='/community' className='side_menus_menu chatbot'>
+            {isMobile ? <span className="icon">🎭🗯</span> : <span className="text">커뮤니티</span>}
+          </Link>
+          <Link to='/profile' className='side_menus_menu chatbot'>
+            {isMobile ? <span className="icon">⚙️</span> : <span className="text">프로필</span>}
+          </Link>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SideMenu
+export default SideMenu;
