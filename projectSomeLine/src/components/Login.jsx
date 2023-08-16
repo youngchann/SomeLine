86cb120478d335d2dc99f680d093a25030e262dc
leechanyongyong <goodleecy1989@gmail.com>
2023-08-16 16:57:46 +0900

    8.16.17.lee

--- projectSomeLine/src/components/Login.jsx
+++ projectSomeLine/src/components/Login.jsx
@@ -35,47 +35,48 @@ const Login = () => {
 
   return (
     <div className='login_page'>
-          <div className={isVisible ? 'main_popup_page' : 'hidden'}>
-            <button className='main_popup_page_close' onClick={closePopup}>X</button>
-            <div className='main_popup_page_text'>
-              <h1>안녕하세요. 반가워요.</h1><br/>
-              <p>저희는 <strong>App</strong>반의 <strong>OS</strong>팀입니다. </p>
-              <p>저희가 제작한 웹서비스는 소개팅 웹입니다.</p>
-              <p>AI를 이용하여 취향을 분석한 뒤 딱 맞는 이성에게 매칭해 드립니다.</p>
-              <p>사랑해 주셔서 감사합니다.</p><br/>
-              <h4>팀원</h4>
-              <p>제작  :  이찬용, 전도희, 국지호, 임영찬</p>
-              <Link to='/teammember'><button className="teammember_button">팀원 소개</button></Link>
-            </div>
-          </div>
-          <div className="login_bgm_b">
-            <video className="login_bgm" autoPlay muted loop>
-              <source src='videos/mainmain9.mp4' type='video/mp4' />
-            </video>
-          </div>
-          <div className='login_box'>
-            <div className="login_box_header_name">
-              <h1>Login</h1>
-            </div>
-            <form onSubmit={handleSubmit} className="loginbox_in_input_box">
-              <input
-              className="id_box"
-              id="id"
-              placeholder="이메일.."
-              ></input>
-              <input
-              className="ps_box"
-              id="password"
-              type="password"
-              placeholder="비밀번호.."
-              
-              ></input>
-              <button className="login_submit_button">Login</button>
-            </form>
-            {/* <a href='#' className="find_ps_id">비밀번호 찾기 / 아이디 찾기</a> */}
+      <div className='communityspace_bg_shadow'><p title='이찬용'>✨</p><p title='전도희'>✨</p><p title='국지호'>✨</p><p title='임영찬'>✨</p></div>
+      <div className={isVisible ? 'main_popup_page' : 'hidden'}>
+        <button className='main_popup_page_close' onClick={closePopup}>X</button>
+        <div className='main_popup_page_text'>
+          <h1>안녕하세요. 반가워요.</h1><br/>
+          <p>저희는 <strong>App</strong>반의 <strong>OS</strong>팀입니다. </p>
+          <p>저희가 제작한 웹서비스는 소개팅 웹입니다.</p>
+          <p>AI를 이용하여 취향을 분석한 뒤 딱 맞는 이성에게 매칭해 드립니다.</p>
+          <p>사랑해 주셔서 감사합니다.</p><br/>
+          <h4>팀원</h4>
+          <p>제작  :  이찬용, 전도희, 국지호, 임영찬</p>
+          <Link to='/teammember'><button className="teammember_button">팀원 소개</button></Link>
         </div>
-        <div className="login_desing_font1">some.</div>
-        <div className="login_desing_font2">line</div>
+      </div>
+      <div className="login_bgm_b">
+        <video className="login_bgm" autoPlay muted loop>
+          <source src='videos/mainmain9.mp4' type='video/mp4' />
+        </video>
+      </div>
+      <div className='login_box'>
+        <div className="login_box_header_name">
+          <h1>Login</h1>
+        </div>
+        <form onSubmit={handleSubmit} className="loginbox_in_input_box">
+          <input
+          className="id_box"
+          id="id"
+          placeholder="이메일.."
+          ></input>
+          <input
+          className="ps_box"
+          id="password"
+          type="password"
+          placeholder="비밀번호.."
+          
+          ></input>
+          <button className="login_submit_button">Login</button>
+        </form>
+          {/* <a href='#' className="find_ps_id">비밀번호 찾기 / 아이디 찾기</a> */}
+      </div>
+      <div className="login_desing_font1">some.</div>
+      <div className="login_desing_font2">line</div>
     </div>
   )
 }
